f627dd497ed7a1c504c8f993c89bfdceca620a97
SHENOISZ <marcelo.net.system@gmail.com>
2016-12-31 19:54:10 -0300

    portfolio

---
+++ assets/js/init.js
@@ -0,0 +1,93 @@
+email = document.querySelector('.email-action');
+	email_text = document.querySelector('.email-text');
+
+	next_section = document.querySelector('.next-section');
+
+	email.addEventListener('click',function(event){
+
+		event.preventDefault();
+
+		email_text.style.opacity = '1';
+	});
+
+	/*
+	var details = document.querySelectorAll('.overlay-job');
+
+	details.forEach(function(detail){
+
+		detail.addEventListener('mousemove',function(event){
+			console.log(event.offsetX+","+event.offsetY);
+		})
+	});
+	*/
+
+	/* ======================== */
+
+	function obj(args, selector) {
+		return args.querySelector(selector);
+	}
+
+	var jobs = document.querySelectorAll('.item-job, .item-job .getpos');
+	var jobs_ = document.querySelectorAll('.item-job');
+	var activated = true;
+
+	jobs.forEach(function(job){
+
+		job.addEventListener('mousemove',function(e) {
+
+			if (e.target.getAttribute('name') == 'top' && activated) {
+
+				var ob = obj(job, '.overlay-job-1').style;
+				ob.top = "0";
+				ob.transition = "0.6s";
+				activated = false;
+
+			} else if (e.target.getAttribute('name') == 'left' && activated) {
+
+				var ob = obj(job, '.overlay-job-2').style;
+				ob.left = "0";
+				ob.transition = "0.6s";
+				activated = false;
+
+			} else if (e.target.getAttribute('name') == 'right' && activated) {
+
+				var ob = obj(job, '.overlay-job-3').style;
+				ob.left = "0";
+				ob.transition = "0.6s";
+				activated = false;
+
+			} else if (e.target.getAttribute('name') == 'bottom' && activated) {
+
+				var ob = obj(job, '.overlay-job-4').style;
+				ob.top = "0";
+				ob.transition = "0.6s";
+				activated = false;
+			}
+
+		});
+	});
+
+	/* jobs reset all  */
+
+	jobs_ .forEach(function(job){
+
+		job.addEventListener('mouseleave',function(e) {
+
+			var ob = null;
+
+			ob = obj(job, '.overlay-job-1').style;
+			ob.top = "-100%";
+			ob.transition = "0.6s";
+			ob = obj(job, '.overlay-job-2').style;
+			ob.left = "-100%";
+			ob.transition = "0.6s";
+			ob = obj(job, '.overlay-job-3').style;
+			ob.left = "100%";
+			ob.transition = "0.6s";
+			ob = obj(job, '.overlay-job-4').style;
+			ob.top = "100%";
+			ob.transition = "0.6s";
+
+			activated = true;
+		});
+	});
